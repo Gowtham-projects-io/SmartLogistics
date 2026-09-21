15d2d75278d0b6b067e50f6aaa060183476de40b
gowtham <gowthamthanigachalam827@gmail.com>
2026-09-22 00:12:19 +0530

    fix: unregister legacy service worker and clear stale cacheStorage

---
+++ frontend/public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function() {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() {
+      return self.registration.unregister();
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
